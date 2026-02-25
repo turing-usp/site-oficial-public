c0c881e84e33cce6da849ee8ed62d9dfd71a1218
Gabriel Silva <gabrielhenrique_alves@hotmail.com>
2026-02-24 21:47:42 -0300

    Mexi na equipe, ainda não resolvido

--- app/(public)/equipe/page.tsx
+++ app/(public)/equipe/page.tsx
@@ -9,35 +9,35 @@ export default async function Equipe(){
 
     return(
         <>
-            <div className="relative flex h-[100vh] w-[100%] items-center justify-center">
-                        <div className="flex flex-1 items-center relative">
+            <div className="relative flex min-h-fit md:h-[100vh] w-full items-start justify-center pt-[20vh] pb-10 overflow-hidden">
+                        <div className="flex flex-1 items-center relative self-center">
                             <Image
                                 src="/Elipse.svg"
                                 alt="imagem elipse"
                                 width={600}
                                 height={600}
-                                className="absolute left-0 h-[70vh] w-auto"
+                                className="absolute left-0 h-[100vh] opacity-0 md:opacity-100 md:h-[70vh] w-auto"
                             />
                             <Image 
                                 src="/Capacete.svg"
                                 alt="imagem capacete"
                                 width={600}
                                 height={600}
-                                className="absolute left-0 h-[40vh] w-auto"
+                                className="absolute left-0 h-[50vh] md:h-[40vh] w-auto"
                             />
                         </div>
-                        <div className="flex flex-1 flex-col items-center justify-center">
+                        <div className="flex flex-1 flex-col items-center justify-center self-center">
                             <p className="text-[1.5rem] text-[#766F6F] font-bold">NOSSA EQUIPE</p>
-                            <p className="text-[4rem] font-bold text-[#000000] text-center">CONSTRUTORES DO FUTURO</p>
-                            <p className="text-[1.5rem] text-[#000000] text-center">Conheça a nossa equipe que ajuda a construir e aplicar conceitos importantes para o aprendizado da Inteligência artificial.</p>
+                            <p className="text-[#000000] text-[2.2rem] md:text-[4rem] text-center font-bold leading-tight px-4 md:px-0">CONSTRUTORES DO FUTURO</p>
+                            <p className="mt-[5%] md:mt-0 text-[#000000] text-center text-[1.2rem] md:text-[1.5rem] leading-tight px-4 md:px-0">Conheça a nossa equipe que ajuda a construir e aplicar conceitos importantes para o aprendizado da Inteligência artificial.</p>
                         </div>
-                        <div className="flex flex-1 items-center relative">
+                        <div className="flex flex-1 items-center relative self-center">
                             <Image
                                 src="/Elipse.svg"
                                 alt="imagem elipse"
                                 width={600}
                                 height={600}
-                                className="absolute rotate-180 right-0 h-[70vh] w-auto"
+                                className="absolute rotate-180 right-0 h-[100vh] opacity-0 md:opacity-100 md:h-[70vh] w-auto"
                             />
                             <Image 
                                 src="/solda.svg"
