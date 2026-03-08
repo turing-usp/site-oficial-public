365e8db7beb0e8e8d0edaf7f9a007e879212299c
Luca Henrique Tavares <160362413+Luca-Henrique-Tavares@users.noreply.github.com>
2026-03-08 16:08:14 -0300

    Mudanças sitemap!

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,42 +4,42 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: "https://turingusp.com",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 1.0,
         },
         {   
             url: "https://turingusp.com/projetos",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/contato",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/equipe",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/turing-talks",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/eventos",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/cadastre-se",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         },
         {
             url: "https://turingusp.com/login",
-            lastModified: new Date(),
+            lastModified: new Date(2026, 2, 7),
             priority: 0.8,
         }
     ];
